2228cb27d9e587986b447d1a8ee91925d40f5c0a
super.pastor <jose.pastor@implementalsystems.com>
2025-09-16 14:18:11 -0600

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection ,importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-        provideRouter(routes),
-        provideHttpClient(),
-        importProvidersFrom(BrowserAnimationsModule)
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
