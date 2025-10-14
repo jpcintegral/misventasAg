b7bb7751101db4ecc6d50ce5f38151300087eab9
jose pastor <jpastorcardenas@gmail.com>
2025-10-13 21:45:24 -0600

    se descarta las variables dentorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   stripePublicKey:
     'pk_test_51RL3UxPE8FwJnpgibE24PfYE8NspjiCeO4r6DWXDu9tFp0pZDjSlh4fuxjaiPWfXvIJkTp0KW5bnpSUVNRRPtdl7005gmJSGcW',
-  urlBackend: 'https://misventascms.devscolima.com',
+  urlBackend: 'http://localhost:1337',
 };
